5865c0917a7b07d2a38098166546feecc8ba5867
Cameron Bowen <39662961+wcameronbowen@users.noreply.github.com>
2021-04-08 23:04:56 -0500

    adding variable for column to monitor

--- clear-zero-row.js
+++ clear-zero-row.js
@@ -1,14 +1,14 @@
-function main(workbook: ExcelScript.Workbook)
-{
+function main(workbook: ExcelScript.Workbook) {
+  
   let range = workbook.getActiveWorksheet().getRange("A1:D34");
   let rangeValues = range.getValues();
+  let monitorColumn = 3
 
   rangeValues.forEach((rowItem, rowIndex) => {
-
     rangeValues[rowIndex].forEach((columnItem, columnIndex) => {
       let columnValue = columnItem as number;
       if (columnValue.valueOf() == 0) {
-        if (columnIndex == 3) {
+        if (columnIndex == monitorColumn) {
           range.getRow(rowIndex).delete(ExcelScript.DeleteShiftDirection.up);
         }
       }
